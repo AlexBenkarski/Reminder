46162f3607fbb24767b119383334418178d717e8
AlexBenkarski <130891155+AlexBenkarski@users.noreply.github.com>
2024-02-07 21:02:53 -0500

    Added the ability to update specific reminders.

--- src/Reminder.java
+++ src/Reminder.java
@@ -27,6 +27,7 @@ public class Reminder implements Serializable {
         return title +
                 "\nDescription: " + description +
                 "\nDateTime: " + dateTime +
+                "\nPriority: " + priority +
                 "\nLocation: " + location + "\n";
     }
 
@@ -114,11 +115,16 @@ public class Reminder implements Serializable {
             System.out.println("Menu:");
             System.out.println("1. Create a reminder");
             System.out.println("2. View all reminders");
-            System.out.println("3. Quit program");
+            System.out.println("3. Edit reminder");
+            System.out.println("4. Quit program");
 
             int choice;
             try {
                 choice = Integer.parseInt(scnr.nextLine());
+                if (choice < 1 || choice > 5) {
+                    System.out.println("Invalid Choice! Please select a number 1-5");
+                    continue;
+                }
             } catch (NumberFormatException e) {
                 System.out.println("Invalid choice. Please enter a valid choice");
                 continue;
@@ -133,26 +139,100 @@ public class Reminder implements Serializable {
                 case 2:
                     if (reminders.isEmpty()) {
                         System.out.println("No reminders available");
-                    }else {
+                    } else {
                         System.out.println("All reminders!");
-                        for(Reminder r : reminders){
+                        for (Reminder r : reminders) {
                             System.out.println(r);
                         }
 
                     }
                     break;
                 case 3:
+                    editReminderMenu(reminders, scnr);
+                break;
+                case 4:
                     saveReminders(reminders);
                     System.out.println("Goodbye!");
                     scnr.close();
                     System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please enter a valid choice");
-                    break;
-            }
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a valid choice");
+                break;
         }
     }
+
+}
+
+    private static void editReminderMenu(List<Reminder> reminders, Scanner scnr) {
+        System.out.println("Reminders: ");
+        for (int i = 0; i < reminders.size(); i++){
+            System.out.println((i + 1) + " . " + reminders.get(i).getTitle());
+        }
+        System.out.println("Enter the number of the reminder you want to edit: ");
+        int selection = Integer.parseInt(scnr.nextLine()) -1;
+        if (selection < 0 || selection >= reminders.size()){
+            System.out.println("Invalid Selection. Enter a valid number: ");
+            return;
+        }
+
+        Reminder selectedReminder = reminders.get(selection);
+        editReminder(selectedReminder, scnr);
+    }
+
+    public static void editReminder(Reminder reminder,Scanner scnr){
+        System.out.println("Editing Reminder: " + reminder.getTitle());
+        System.out.println("1. Edit reminder name: ");
+        System.out.println("2. Edit reminder description: ");
+        System.out.println("3. Edit reminder date and time: ");
+        System.out.println("4. Edit reminder location: ");
+        System.out.println("5. Edit reminder priority: ");
+
+        int editChoice;
+        try {
+            editChoice = Integer.parseInt(scnr.nextLine());
+            if (editChoice < 1 || editChoice > 5) {
+                System.out.println("Invalid Choice! Please select a number 1-5");
+                return;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid choice. Please enter a valid number.");
+            return; // Return to the main menu
+        }
+
+        switch (editChoice) {
+            case 1:
+                System.out.println("Enter a new title: ");
+                String newTitle = scnr.nextLine();
+                reminder.setTitle(newTitle);
+                break;
+            case 2:
+                System.out.println("Enter a new description: ");
+                String newDescription = scnr.nextLine();
+                reminder.setDescription(newDescription);
+                break;
+            case 3:
+                System.out.println("Enter a new date and time in the format (yyyy-MM-dd HH:mm): ");
+                LocalDateTime newDateTime = LocalDateTime.parse(scnr.nextLine());
+                reminder.setDateTime(newDateTime);
+                break;
+            case 4:
+                System.out.println("Enter a new location: ");
+                String newLocation = scnr.nextLine();
+                reminder.setLocation(newLocation);
+                break;
+            case 5:
+                System.out.println("Enter a new priority");
+                Priority newPriority = Priority.valueOf(scnr.nextLine());
+                reminder.setPriority(newPriority);
+                break;
+
+            default:
+                System.out.println("Invalid choice. Please enter a valid number between 1 and 5.");
+                break;
+        }
+    }
+
     private static List<Reminder> readRemindersFromFile(){
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("reminder.txt"))){
             return (List<Reminder>) in.readObject();
